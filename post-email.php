6de791538fbb75f460cae92d65aa8c9b7ad0fd2d
oljaja2 <115540602+oljaja2@users.noreply.github.com>
2022-12-06 14:26:35 +0300

    Add files via upload
    send message

--- post-email.php
+++ post-email.php
@@ -1,15 +1,10 @@
 <?php
-if (!$_POST) exit;
 $email = trim(strip_tags($_POST['email']));
 $name = trim(strip_tags($_POST['name']));
-$name = trim(strip_tags($_POST['tel']));
+$tel = trim(strip_tags($_POST['tel']));
 $to = 'oljaja2@gmail.com'; // адрес получателя
-$subject = 'Новый клиент'; // тема письма
-// формируем тело сообщения
-$message = 'Имя: ' . $name . 'Email: ' . $email; 
-// формируем headers для письма
-$headers = 'From: '. $email ; // от кого
-// отправка
-@mail($to, $subject, $message, $headers);
-echo 'Спасибо, обращение отправлено!';
+$subject = 'Новый клиент '; // тема письма
+$message = 'Имя: ' . $name . ' ' . 'Email: ' . $email . ' ' . 'телефон: ' . $tel; // формируем тело сообщения
+mail("$to", "$subject", "$message", "$headers");
+
 ?>
